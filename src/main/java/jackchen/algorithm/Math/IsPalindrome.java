35c864b269f76c8bab67775e0ff99aaae21c6b3d
Chao Chen <jackchen.drinlee@gmail.com>
2018-05-22 00:00:41 -0700

    update list problems

---
+++ src/main/java/jackchen/algorithm/Math/IsPalindrome.java
@@ -0,0 +1,11 @@
+package jackchen.algorithm.Math;
+
+public class IsPalindrome {
+
+    /*
+     * 9. Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+     */
+    public boolean isPalindrome(int x) {
+        if (x < 0 || ())
+    }
+}
---
+++ src/main/java/jackchen/algorithm/Math/Reverse.java
@@ -0,0 +1,25 @@
+package jackchen.algorithm.Math;
+
+public class Reverse {
+
+    /*
+     * 7. Given a 32-bit signed integer, reverse digits of an integer.
+     */
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0)
+        {
+            int tail = x % 10;
+            int newResult = result * 10 + tail;
+
+            if ((newResult - tail) / 10 != result)
+            { return 0; }
+
+            result = newResult;
+
+            x = x / 10;
+        }
+
+        return result;
+    }
+}
