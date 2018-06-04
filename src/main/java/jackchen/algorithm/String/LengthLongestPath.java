97e9def58cfe96e19d9200c3b8d79491c221aea6
Chao Chen <jackchen.drinlee@gmail.com>
2018-06-03 23:48:22 -0700

    Update problems

---
+++ src/main/java/jackchen/algorithm/String/LengthLongestPath.java
@@ -0,0 +1,14 @@
+package jackchen.algorithm.String;
+
+public class LengthLongestPath {
+
+    /*
+     * 388. The string "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+     * Given a string representing the file system in the above format, return the length of the longest absolute path
+     * to file in the abstracted file system. If there is no file in the system, return 0.
+     */
+    public int lengthLongestPath(String input) {
+        
+    }
+}}
+}
