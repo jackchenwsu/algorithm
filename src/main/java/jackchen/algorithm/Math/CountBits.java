c3dcb9cd6e373bbd6d23a7c57e8bf983817c878d
Chao Chen <jackchen@MacBook-Pro.local>
2018-07-17 21:09:45 -0700

    Add CountBits problem

---
+++ src/main/java/jackchen/algorithm/Math/CountBits.java
@@ -0,0 +1,28 @@
+package jackchen.algorithm.Math;
+
+public class CountBits {
+
+    /*
+     * 338. Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+     */
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        for (int i = 0; i <= num; ++i)
+            ans[i] = popcount(i);
+        return ans;
+    }
+
+    public int[] countBits2(int num) {
+        int[] ans = new int[num + 1];
+        for (int i = 1; i <= num; ++i)
+            ans[i] = ans[i >> 1] + (i & 1); // x / 2 is x >> 1 and x % 2 is x & 1
+        return ans;
+    }
+
+    private int popcount(int x) {
+        int count;
+        for (count = 0; x != 0; ++count)
+            x &= x - 1; //zeroing out the least significant nonzero bit
+        return count;
+    }
+}
