cf8ec492db1ce53d196e8807469f602a94c55d3f
Chao Chen <jackchen.drinlee@gmail.com>
2018-08-01 23:14:38 -0700

    Update problems

---
+++ src/main/java/jackchen/algorithm/array/CanPartition.java
@@ -0,0 +1,31 @@
+package jackchen.algorithm.array;
+
+public class CanPartition {
+
+    /*
+     * 416. Given a non-empty array containing only positive integers,
+     *      find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+     */
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums)
+        {
+            sum += num;
+        }
+
+        if (sum%2 == 1)
+            return false;
+
+        sum /= 2;
+
+        boolean[] dp = new boolean[sum+1];
+        dp[0] = true;
+        for (int num : nums) {
+            for (int i = sum; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+
+        return dp[sum];
+    }
+}
