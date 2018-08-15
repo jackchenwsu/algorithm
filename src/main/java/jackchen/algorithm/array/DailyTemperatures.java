bdb14fca72073dbb69d4c79d279ec01693b1b8f8
Chao Chen <jackchen.drinlee@gmail.com>
2018-08-14 21:08:37 -0700

    Update problems

---
+++ src/main/java/jackchen/algorithm/array/DailyTemperatures.java
@@ -0,0 +1,26 @@
+package jackchen.algorithm.array;
+
+public class DailyTemperatures {
+
+    /*
+     * 739. Given a list of daily temperatures, produce a list that, for each day in the input,
+     *      tells you how many days you would have to wait until a warmer temperature.
+     *      If there is no future day for which this is possible, put 0 instead.
+     *      For example, given the list temperatures = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+     */
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i= temperatures.length - 1; i >= 0; i--) {
+
+            while(!stack.isEmpty() && temperatures[i] >= T[stack.peek()]) stack.pop();
+
+            res[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+
+            stack.push(i);
+        }
+
+        return res;
+    }
+}
