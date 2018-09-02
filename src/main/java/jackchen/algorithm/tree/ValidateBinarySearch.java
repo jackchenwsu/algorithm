2b16e86efcd8967ee8a56af6c8e6b9ba5e1ff1a8
Chao Chen <jackchen.drinlee@gmail.com>
2018-09-01 22:38:39 -0700

    Update tree problems

---
+++ src/main/java/jackchen/algorithm/tree/ValidateBinarySearch.java
@@ -0,0 +1,17 @@
+package jackchen.algorithm.tree;
+
+public class ValidateBinarySearch {
+
+    /*
+     * 98. Given a binary tree, determine if it is a valid binary search tree (BST).
+     */
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+}
