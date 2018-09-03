95b8b161d249e116b737e084225544b1f5eb634d
Chao Chen <jackchen.drinlee@gmail.com>
2018-09-03 12:10:25 -0700

    Update problems 09/03/2018

---
+++ src/main/java/jackchen/algorithm/List/ReverseLinkedListII.java
@@ -0,0 +1,11 @@
+package jackchen.algorithm.List;
+
+public class ReverseLinkedListII {
+
+    /*
+     * 92. Reverse a linked list from position m to n. Do it in one-pass.
+     */
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        return null;
+    }
+}
--- src/main/java/jackchen/algorithm/array/MultiplyStrings.java
+++ src/main/java/jackchen/algorithm/array/MultiplyStrings.java
@@ -1,7 +1,10 @@
 package jackchen.algorithm.array;
 
 public class MultiplyStrings {
-
+    /*
+     * 43. Given two non-negative integers num1 and num2 represented as strings,
+     *     return the product of num1 and num2, also represented as a string.
+     */
     public String multiply(String num1, String num2) {
         int n = num1.length(), m = num2.length();
         int [] value = new int[n+m];
